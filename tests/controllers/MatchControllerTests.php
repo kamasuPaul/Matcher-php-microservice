4e523bfdb3e8d62cfa5c47304c0d94aec4621e66
Kamasu Paul <kamasupaul@gmail.com>
2022-02-22 10:07:45 +0300

    -- add match controller tests class

---
+++ tests/controllers/MatchControllerTests.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Controllers;
+
+use Illuminate\Http\Response;
+use Tests\TestCase;
+
+class MatchControllerTests extends TestCase {
+    public function test_match_endpoint_returns_data_in_valid_format(){
+        $property_id = 1;
+        $response = $this->get("/api/match/$property_id");
+        $response->assertStatus(Response::HTTP_OK);
+    }
+}
